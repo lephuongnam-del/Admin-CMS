cba19db3682b5a5384bcfca8c42fe92481d12092
youngboy <youngboy111999@gmail.com>
2022-10-02 15:52:21 +0700

    create table for db

--- admin-cms/src/api/product-detail/services/product-detail.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * product-detail service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::product-detail.product-detail');
